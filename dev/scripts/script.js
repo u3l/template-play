b59c091eb51a35041cc1dece73d55cbbefc16713
Michael Whyte <info@mwhyte.ca>
2017-11-12 15:21:51 -0800

    Create starter files

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,1 +1,14 @@
 // Insert JavaScript here...
+
+const username = 'Michael';
+
+console.log(sayName(username) );
+
+const $paragraph = $('section:nth-of-type(1) p:nth-of-type(2)');
+
+$paragraph.text(`${sayName(username)}! This text was changed by jQuery.`);
+
+
+function sayName(name){
+    return `Hello ${name}`;
+}
